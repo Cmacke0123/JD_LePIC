e786d874248a30ac2899578d5e0e2021fc4f0073
Connor MacKenize <connordmackenzie02@gmail.com>
2026-05-28 16:13:53 -0600

    Moving updates to fork and added Jasmins' repo as upstream

--- Src/main.f90
+++ Src/main.f90
@@ -491,7 +491,7 @@ program main
      tseq= nseq*dt ! correct tseq
   endif
   nudt=nu_h*(ns_heat*dt)  ! % of electrons which should be heated
-  nu_uplim(1)=4.d7 ! Maximum collision frequency (e-)
+  nu_uplim(1)=6.d7 ! Maximum collision frequency (e-)
   nu_uplim(2:ntype)=5.d6 ! (ions)
   ns_Hm= 1 ! Negative ion injection frequency on the PE
   eheat_type=1 ! Electron velocity sampled from Maxwellian: 1= full velocity, 2= only increment dv
